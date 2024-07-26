c22fdee0a72b7a7d11f38b4de669e681869b8e7d
Robson <rgolobovante@gmail.com>
2024-07-26 00:04:52 -0300

    :sparkles: Criando classe conta bancaria

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        contaBancaria contabancaria = new contaBancaria();
+
+        //TODO: COnhecer e importar a classe Scanner
+        
+        Scanner scan = new Scanner(System.in);
+
+        //Exibir as mesagens para nosso usuario
+        //obter pelo scanner os valores digitados no terminal
+        System.out.println("Por favor, digite o número da conta !");
+        contabancaria.setNumero(scan.nextInt());
+        System.out.println("Por favor, digite o número da Agência !");
+        contabancaria.setAgencia(scan.next());
+        System.out.println("Por favor, digite seu nome !");
+        contabancaria.setNomeCliente(scan.next());
+        System.out.println("Por favor, digite seu saldo !");
+        contabancaria.setSaldo(scan.nextDouble());
+        
+
+        //Exibir a mensagem conta criada
+        System.out.println("Olá " + contabancaria.getNomeCliente() + 
+                           ", obrigado por criar uma conta em nosso banco, sua agência é " +
+                            contabancaria.getAgencia() +
+                           ", conta " +  contabancaria.getNumero() + " e seu saldo " + 
+                            contabancaria.getSaldo() + " já está disponível para saque");
+                            
     }
 }
---
+++ conta-banco/src/contaBancaria.java
@@ -0,0 +1,45 @@
+public class contaBancaria {
+    private int numero;
+    private String agencia;
+    private String nomeCliente;
+    private double saldo;
+
+    public int getNumero() {
+        return numero;
+    }
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+    public String getAgencia() {
+        return agencia;
+    }
+    public void setAgencia(String agencia) {
+        this.agencia = agencia;
+    }
+    public String getNomeCliente() {
+        return nomeCliente;
+    }
+    public void setNomeCliente(String nomeCliente) {
+        this.nomeCliente = nomeCliente;
+    }
+    public double getSaldo() {
+        return saldo;
+    }
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+    @Override
+    public String toString() {
+        return "contaBancaria [numero=" + numero + ", agencia=" + agencia + ", nomeCliente=" + nomeCliente + ", saldo="
+                + saldo + "]";
+    }
+
+    
+
+
+
+
+
+    
+    
+}
